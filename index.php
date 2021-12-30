47aab98dd59bf2c72d069ea8ed7a03e34035ae1e
Izul Wahidin <izul@wahid.in>
2021-12-30 20:33:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
 require('bca.php');
-define("mutasi_bca",[
-    'user_id' => 'username_bca',
-    'pswd' => 'pass_bca',
-]);
+define("mutasi_bca",
+    [
+        'user_id' => 'username_bca',
+        'pswd' => 'pass_bca'
+    ]
+);
 
 $date = 30; // max 30 hari
 $bca = new BCA(
-  mutasi_bca['user_id'],
-  mutasi_bca['pswd'],
-  $date
+    mutasi_bca['user_id'],
+    mutasi_bca['pswd'],
+    $date
 );
 
 $mutasi = $bca->mutasiTrx();
